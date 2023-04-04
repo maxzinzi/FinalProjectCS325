5fc4407658dcb4881ca8330c4f05b19680bfa64a
JMarion2 <jmarion@siue.edu>
2023-04-04 00:09:37 -0500

    removed old homepage, and did userstories

---
+++ project 3/homepage.js
@@ -0,0 +1,16 @@
+// Snake
+const snakeTutorialBtn = document.getElementById('snake-tutorial-btn');
+const snakeTutorialPopup = document.getElementById('snake-tutorial-popup');
+
+snakeTutorialBtn.addEventListener('click', () => {
+    snakeTutorialPopup.style.display = 'block';
+});
+
+  snakeTutorialPopup.addEventListener('click', () => {
+    snakeTutorialPopup.style.display = 'none';
+});
+
+// Frogger
+
+
+// Doodle Jump
