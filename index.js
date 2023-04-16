d2e23d70ddfb26cfacf1c18b1e450abe914829be
MaxwellZinzilieta <maxwellrzinzi@gmail.com>
2023-04-15 22:24:58 -0500

    check login

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ event.preventDefault();
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     if(checkExistingUser(username)) {
-        alert(`Account with username: ${username} already exists!`);
+        alert(`Account with username ${username} already exists!`);
     }
     else {
-        alert("idk i just needed to put something in the else part");
+        createUser(username, password);
     }
 });
 
